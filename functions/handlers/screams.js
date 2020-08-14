c507d78bb6c5521058dc1cff4230cab15305a875
tiennvn0 <tiennvn0@gmai.com>
2020-08-14 11:20:44 +0700

    ha xuong node 8, fix gcloud, getAllScreams

---
+++ functions/handlers/screams.js
@@ -0,0 +1,26 @@
+const { db } = require("../util/admin");
+
+exports.getAllScreams = (req, res) => {
+  db.collection("screams")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then((data) => {
+      let screams = [];
+      data.forEach((doc) => {
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+          commentCount: doc.data().commentCount,
+          likeCount: doc.data().likeCount,
+          userImage: doc.data().userImage,
+        });
+      });
+      return res.json(screams);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: err.code });
+    });
+};
--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,17 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const app = require("express")();
+
+const { getAllScreams } = require("./handlers/screams");
+
+// Scream routes
+app.get("/screams", getAllScreams);
+
+exports.api = functions.https.onRequest(app);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Firebase!");
+});
